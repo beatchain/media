afb23e46ad8807bf34f1434d7b6ee18fbafb9a61
edgodshaw <ed.godshaw@beatchain.com>
2024-10-29 17:13:52 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+var ffmpeg = require('fluent-ffmpeg');
+var command = ffmpeg();
+
 const port = 3001
 
+
 app.get('/', (req, res) => {
+  console.log(command)
   res.send('Hello World')
 })
 
